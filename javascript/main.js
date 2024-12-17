ad767a7e13d0ca7a39e7946cb855cf4fec2d2e99
Arianit173 <arianitvllasaliu@gmail.com>
2024-12-17 07:13:11 -0800

    ndryshime

---
+++ javascript/main.js
@@ -0,0 +1,20 @@
+console.log('Hello World')
+
+// var x=10;
+// var value= "digitalschool"
+
+var a=50;
+var b=30;
+var n=a*b;
+var N=null;
+console.log(n);
+
+var c=10;
+var d=50;
+var e=c+d;
+console.log(e);
+
+var t=99;
+t++;
+console.log(t);
+
