320feebe8901319dc6d31aa0168868e764ac9a07
Arianit173 <arianitvllasaliu@gmail.com>
2024-12-19 07:11:12 -0800

    ndyshime

--- javascript/main.js
+++ javascript/main.js
@@ -3,18 +3,85 @@ console.log('Hello World')
 // var x=10;
 // var value= "digitalschool"
 
-var a=50;
-var b=30;
-var n=a*b;
-var N=null;
-console.log(n);
+// var a=50;
+// var b=30;
+// var n=a*b;
+// var N=null;
+// console.log(n);
 
-var c=10;
-var d=50;
-var e=c+d;
-console.log(e);
+// var c=10;
+// var d=50;
+// var e=c+d;
+// console.log(e);
 
-var t=99;
-t++;
-console.log(t);
+// var t=99;
+// t++;
+// console.log(t);
+
+// var t=101;
+// t--;
+// console.log(t);
+
+// var e=25;
+// var r= e **5
+// console.log(r);
+
+
+
+// var x=25;
+// x +=5;
+// console.log(x);
+
+// var y=25;
+// y -=5;
+// console.log(y);
+
+// var o=15;
+// var p= o-5;
+// console.log(p);
+ 
+// var e=50;
+// e /=10;
+// console.log(e);
+
+// var t=40;
+// var w=t/4;
+// console.log(w);
+
+
+// var x=5;
+// var y=5;
+// var num =(x === y);
+// console.log(num);
+
+// var num1=60;
+// var num2=50;
+// var x=(num2 >= num1);
+// console.log(x);
+
+// var e=40;
+// var y=70;
+// var p=(e <= y);
+// console.log(p)
+// var firstName= 'Arianit';
+// var lastName='Vllasaliu';
+// var fullName= firstName  +  lastName;
+// console.log(fullName);
+
+// var var3=9;
+// var var4=10;
+// var var5=( var3==var4);
+// console.log(var5);
+
+// var age=23;
+// console.log(age >=22 && age <=65);
+
+
+
+
+
+
+
+
+
 
