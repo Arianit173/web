77f05f31d009c5b65d6edbe775ac3f5339c5c4d8
Arianit173 <arianitvllasaliu@gmail.com>
2025-02-13 07:13:54 -0800

    ndryshime

--- javascript/lesson35.js
+++ javascript/lesson35.js
@@ -40,3 +40,11 @@ function search(){
     }
 }
 
+function openMenu(){
+    var menu =document.getElementById('myLinks');
+    if(menu.style.display === 'block'){
+        menu.style.display='none';
+    }else{
+        menu.style.display='block';
+    }
+}
